90640d1e175f1fa0ccbc63d4398be910657384a0
ysemenyuk <invent@list.ru>
2021-03-17 15:39:32 +0300

    test rolbar

--- src/initRollbar.js
+++ src/initRollbar.js
@@ -3,6 +3,9 @@ import Rollbar from 'rollbar';
 const accessToken = process.env.POST_CLIENT_ITEM_ACCESS_TOKEN;
 const environment = process.env.NODE_ENV || 'development';
 
+console.log('Rollbar process.env.POST_CLIENT_ITEM_ACCESS_TOKEN', process.env.POST_CLIENT_ITEM_ACCESS_TOKEN);
+console.log('Rollbar accessToken', accessToken);
+
 export default () => new Rollbar({
   accessToken,
   captureUncaught: true,
